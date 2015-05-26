d51f5dae3a3631583587717e6dab8b4fec441226
Ezequiel <egazzera@hotmail.com>
2015-05-26 12:00:05 -0300

    1

---
+++ LatinCMS/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using LatinCMS.Models;
+using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LatinCMS.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            Configuration cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Config).Assembly);
+            ISession session = cfg.BuildSessionFactory().OpenSession();
+            session.BeginTransaction();
+            List<Config> configs = (List<Config>)session.CreateCriteria(typeof(Config)).List<Config>();
+            session.Transaction.Commit();
+            session.Close();
+
+            return View();
+        }
+
+    }
+}
---
+++ LatinCMS/Models/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LatinCMS.Models
+{
+    public class Config
+    {
+        public virtual int Id { get; set; }
+        public virtual int Clave { get; set; }
+        public virtual int Valor { get; set; }
+
+    }
+}
